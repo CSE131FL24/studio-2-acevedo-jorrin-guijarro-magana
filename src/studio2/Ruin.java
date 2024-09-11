6833c6c6eba2c45a607bdb5b73d85e7a0d7fc467
lizbethguijarro <lizbethguijarro@lizbeths-mbp.lan>
2024-09-11 15:14:26 -0500

    studio

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,26 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		
+	Scanner in = new Scanner(System.in);
+		
+	System.out.println("Enter Start Amount):");
+		double startAmount= in.nextDouble ();
+	System.out.println("The win probability:");
+		double winChance= in.nextDouble ();
+	System.out.println("Enter win amount");	
+		double winLimit= in.nextDouble ();
+		
+		while (currentAmountstartAmount < winLimit;
+		
+
+	
+
+		
+	}
+
+}
