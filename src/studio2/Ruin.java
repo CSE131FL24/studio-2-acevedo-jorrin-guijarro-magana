a4d28ee5b41fbe8ab5a70804a0637c2d97b3c78c
lauren <lauren@DESKTOP-I33DL7A.dhcp.wustl.edu>
2024-09-11 15:45:32 -0500

    for eric and liz <3 mis amores

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -5,33 +5,44 @@ import java.util.Scanner;
 public class Ruin {
 
 	public static void main(String[] args) {
-		
-	Scanner in = new Scanner(System.in);
-		
-	System.out.println("Enter Start Amount):");
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter Start Amount):");
 		double startAmount= in.nextDouble ();
-	System.out.println("The win probability:");
+		System.out.println("The win probability:");
 		double winChance= in.nextDouble ();
-	System.out.println("Enter win amount");	
+		System.out.println("Enter win amount");	
 		double winLimit= in.nextDouble ();
-		
+
 		double currentAmount = startAmount;
 		double playerChance = Math.random();
-		
-	while (currentAmount> 0 && currentAmount < winLimit) {
-		//keep gambling
-		if (playerChance <= winChance) {
-			//win & reward
-		} else if (playerChance > winChance) {
-			//lose & money loss
+
+		while (currentAmount> 0 && currentAmount < winLimit) {
+			//keep gambling
+			playerChance = Math.random();
+			
+			if (playerChance <= winChance) {
+				//win & reward
+				currentAmount++;
+				System.out.println("You won $1");
+			} else if (playerChance > winChance) {
+				//lose & money loss
+				currentAmount--;
+				System.out.println("You lost $1");
+			}
+
 		}
-		
-	}
-		
 
-	
+		if (currentAmount <= 0) {
+			System.out.println("You are ruined!");
+		} else if (currentAmount >= winLimit) {
+			System.out.println("You won!");
+		}
 
-		
+
+
+
 	}
 
 }
